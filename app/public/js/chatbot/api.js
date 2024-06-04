0c6da35634eb6c97311d19f4eccf35c2a27e5029
Rafa Bonilla <rafa_lara@hotmail.es>
2024-06-05 00:38:24 +0200

    error api key

--- app/public/js/chatbot/api.js
+++ app/public/js/chatbot/api.js
@@ -1,4 +1,4 @@
-const API_KEY_CHATGPT = "08a027701fmsh81e6d4c9fe56b68p10afc8jsn1f83d9b96d7e";
+//const API_KEY_CHATGPT = ""; Uncomment that lane and adding your key!
 
 export const URL_CHATGPT =
     "https://chatgpt-best-price.p.rapidapi.com/v1/chat/completions";
